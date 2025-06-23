c0bc6c12a932e43d8c58502ae34ff3dcdcacfa8e
rijalhamami22 <masrijal077@gmail.com>
2025-06-23 10:43:11 +0700

    Add files via upload

---
+++ SIMRSPS/main 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int 2,3,4,5;
+
+int main(int argc, char** argv) {
+	
+	cout << "" >>
+	
+	return 0;
+}
---
+++ SIMRSPS/main soal no 5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int 2,3,5,7,11;
+	cout<< "tampilkan bilangan prima!!">>;
+	cout<< "-------------------------">>;
+	return 0;
+}
